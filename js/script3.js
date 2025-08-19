b1fe6d15454fd96330a475a10055336994f36f60
Viraj <viraj@apples-MacBook-Pro-2.local>
2025-08-19 18:10:50 +0530

    First Push of latest Website

---
+++ js/script3.js
@@ -0,0 +1,10 @@
+let slides = document.querySelectorAll('.slide');
+let current = 0;
+
+function showNextSlide() {
+    slides[current].classList.remove('active');
+    current = (current + 1) % slides.length;
+    slides[current].classList.add('active');
+}
+
+setInterval(showNextSlide, 3000); // auto-change every 3 seconds
